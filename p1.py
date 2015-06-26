548c0d59f95f890de596f2bdd1367818c84ae34b
nimeshlk <nimeshlk@gmail.com>
2015-06-25 21:30:30 -0700

    two new files added to repo

---
+++ p1.py
@@ -0,0 +1,17 @@
+print ("mother fucker!")
+
+x = input("enter name ")
+print ("hey " + x)
+
+y = input("jerked off today? ")
+
+nima = ("Hey %s, how is your %s")
+x= input ("name: ")
+y= input ("body part ")
+
+print (nima % x)
+
+
+
+
+input("Press<enter>")
---
+++ p2.py
@@ -0,0 +1,7 @@
+print ("bullshit is full")
+
+print ("more shit")
+print
+
+
+input("press<enter>")
